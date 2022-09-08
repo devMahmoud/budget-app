a97eeb4e63c70ba19b88fa1c1e514d0237443949
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-08 18:57:48 +0200

    Finalize home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
   def index
-    # if user_signed_in?
-    # @categories = current_user.groups
-    # render 'categories/index'
-    # else
-    render 'home/index'
-    # end
+     if user_signed_in?
+      redirect_to groups_path
+     end
   end
 end
