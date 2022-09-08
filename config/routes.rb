c6b290c55581752da7e493381bc3cf740bf0aaff
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-08 09:05:16 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
+  resources :home, only: [:index]
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :operations, only: [:index, :new, :create]
+  end
 end
