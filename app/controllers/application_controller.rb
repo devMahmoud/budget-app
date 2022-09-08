5d73564b6b8962b421b90792f87559ee2b67afef
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-08 18:12:27 +0200

    Add devise to application controller and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
-  # @current_user = User.first
-  def current_user
-    @current_user = User.first
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
