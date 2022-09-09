89b1eaa25c00ca6058f028f20de726d9c9dc2aa3
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-09 20:01:25 +0200

    Remove index action from operations controller

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,21 +1,17 @@
 class OperationsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @operations = Group.find(params[:group_id]).operations
-  end
-
   def new
     @operation = Operation.new
   end
 
   def create
+    puts "my params: #{params.inspect}"
     @operation = Operation.new(operation_params)
     @operation.author_id = current_user.id
-    @group = Group.find(params[:group_id])
     if @operation.save
-      GroupOperation.create(group_id: @group.id, operation_id: @operation.id)
-      redirect_to group_operations_path,
+      GroupOperation.create(group_id: params[:group_id], operation_id: @operation.id)
+      redirect_to group_path(id: params[:group_id]),
                   flash: { success: 'Transaction created successfully.' }
     else
       redirect_to new_group_operation_path, flash: { error: @operation.errors.full_messages }
