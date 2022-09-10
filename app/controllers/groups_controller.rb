f6234210bfe86b43b30fdbd00e4db620a5db4113
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-10 03:11:05 +0200

    Fix navbar styling

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,6 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.where(author_id: current_user.id)
-    # @groups = @user.groups.includes(:author)
   end
 
   def show
